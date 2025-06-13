691f4c3cf8e8c735d5ccfa52aa8fc3d16a3ea774
Andrew Osmond <aosmond@gmail.com>
2025-06-13 10:42:30 -0400

    log fixes

--- dom/media/ipc/RemoteMediaManagerParent.cpp
+++ dom/media/ipc/RemoteMediaManagerParent.cpp
@@ -334,11 +334,11 @@ RemoteMediaManagerParent::RecvOnSetCurrent(const SurfaceDescriptorGPUVideo& aSD)
   const SurfaceDescriptorRemoteDecoder& sd = aSD;
   RefPtr<Image> image = mImageMap[sd.handle()];
   if (!image) {
-    LOG("RemoteMediaManagerParent::RecvOnSetCurrent -- no image " PRIu64, sd.handle());
+    LOG("RemoteMediaManagerParent::RecvOnSetCurrent -- no image %" PRIu64, sd.handle());
     return IPC_OK();
   }
 
-  LOG("RemoteMediaManagerParent::RecvOnSetCurrent -- image " PRIu64  "-> %p (format %d)", sd.handle(), image.get(), static_cast<int>(image->GetFormat()));
+  LOG("RemoteMediaManagerParent::RecvOnSetCurrent -- image %" PRIu64  "-> %p (format %d)", sd.handle(), image.get(), static_cast<int>(image->GetFormat()));
   image->OnSetCurrent();
   return IPC_OK();
 }
