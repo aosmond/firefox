faa8413e9cb690c6e99f248b058723788cffc111
Andrew Osmond <aosmond@gmail.com>
2025-06-17 23:01:54 -0400

    Bug 1972902 - Part 2. Streamline hardware video support configuration in ffmpeg.
    Differential Revision: https://phabricator.services.mozilla.com/D254283

--- dom/media/platforms/ffmpeg/FFmpegDataDecoder.h
+++ dom/media/platforms/ffmpeg/FFmpegDataDecoder.h
@@ -40,8 +40,10 @@ class FFmpegDataDecoder<LIBAV_VER>
   RefPtr<FlushPromise> Flush() override;
   RefPtr<ShutdownPromise> Shutdown() override;
 
-  static AVCodec* FindAVCodec(FFmpegLibWrapper* aLib, AVCodecID aCodec);
+  static AVCodec* FindSoftwareAVCodec(FFmpegLibWrapper* aLib, AVCodecID aCodec);
+#ifdef MOZ_USE_HWDECODE
   static AVCodec* FindHardwareAVCodec(FFmpegLibWrapper* aLib, AVCodecID aCodec);
+#endif
 
  protected:
   // Flush and Drain operation, always run
