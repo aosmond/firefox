335f3c9052a0ada15db9613549e0d98a7b0e3246
Andrew Osmond <aosmond@gmail.com>
2025-07-15 16:00:26 -0400

    build fixup

--- mfbt/Casting.h
+++ mfbt/Casting.h
@@ -101,7 +101,8 @@ T2S(int32_t);
 T2S(int64_t);
 T2S(char16_t);
 T2S(char32_t);
-#if defined(XP_DARWIN) || defined(XP_WIN) || defined(__wasm__) || defined(ANDROID)
+#if defined(XP_DARWIN) || defined(XP_WIN) || defined(__wasm__) || \
+    defined(ANDROID)
 T2S(long);
 T2S(unsigned long);
 #endif
