d434e6e27fc3a5dc360145f41ee1992387b194f2
pommicket <pommicket@gmail.com>
2025-10-22 17:24:52 -0400

    Bug 1994898 - Implement data transfer for ImageDecoder. r?aosmond
    Allows constructing an ImageDecoder object without copying
    encoded image data.
    
    Differential Revision: https://phabricator.services.mozilla.com/D269301

--- image/SourceBuffer.h
+++ image/SourceBuffer.h
@@ -325,6 +325,10 @@ class SourceBuffer final {
   /// Append the data available on the provided nsIInputStream to the buffer.
   nsresult AppendFromInputStream(nsIInputStream* aInputStream, uint32_t aCount);
 
+  /// Take ownership of provided data and append it without copying.
+  nsresult AdoptData(char* aData, size_t aLength,
+                     void* (*aRealloc)(void*, size_t), void (*aFree)(void*));
+
   /**
    * Mark the buffer complete, with a status that will be available to
    * consumers. Further calls to Append() are forbidden after Complete().
@@ -383,7 +387,16 @@ class SourceBuffer final {
       mData = static_cast<char*>(malloc(mCapacity));
     }
 
-    ~Chunk() { free(mData); }
+    /// Create an "adopted" chunk taking ownership of the provided data.
+    Chunk(char* aData, size_t aLength, void* (*aRealloc)(void*, size_t),
+          void (*aFree)(void*))
+        : mCapacity(aLength),
+          mLength(aLength),
+          mData(aData),
+          mRealloc(aRealloc),
+          mFree(aFree) {}
+
+    ~Chunk() { mFree(mData); }
 
     Chunk(Chunk&& aOther)
         : mCapacity(aOther.mCapacity),
@@ -419,7 +432,7 @@ class SourceBuffer final {
 
     bool SetCapacity(size_t aCapacity) {
       MOZ_ASSERT(mData, "Allocation failed but nobody checked for it");
-      char* data = static_cast<char*>(realloc(mData, aCapacity));
+      char* data = static_cast<char*>(mRealloc(mData, aCapacity));
       if (!data) {
         return false;
       }
@@ -436,6 +449,8 @@ class SourceBuffer final {
     size_t mCapacity;
     size_t mLength;
     char* mData;
+    void* (*mRealloc)(void*, size_t) = realloc;
+    void (*mFree)(void*) = free;
   };
 
   nsresult AppendChunk(Maybe<Chunk>&& aChunk) MOZ_REQUIRES(mMutex);
