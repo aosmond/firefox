d5aa81bfc5a99fddb5f2ecb18bf7d071cbd560b9
Andrew Osmond <aosmond@gmail.com>
2025-11-01 22:13:38 -0400

    Bug 1900626 - Allow Android to use our bundled clearkey CDM.
    This patch makes it so that we include libclearkey.so with our APK so
    that our existing GMP plumbing can launch using that library on Android.
    On other platforms we have a specific plugin file system layout that we
    follow and unfortunately that is not easy to do on Android. As such, we
    manually setup the plugin and force the GMP plugin process to find/load
    the library from our environment instead of using the absolute path.
    
    Differential Revision: https://phabricator.services.mozilla.com/D270967

--- dom/media/gmp/GMPChild.cpp
+++ dom/media/gmp/GMPChild.cpp
@@ -240,6 +240,9 @@ mozilla::ipc::IPCResult GMPChild::RecvPreloadLibs(const nsCString& aLibs) {
 }
 
 bool GMPChild::GetUTF8LibPath(nsACString& aOutLibPath) {
+#ifdef MOZ_WIDGET_ANDROID
+  aOutLibPath = "lib"_ns + NS_ConvertUTF16toUTF8(mPluginPath) + ".so"_ns;
+#else
   nsCOMPtr<nsIFile> libFile;
 
 #define GMP_PATH_CRASH(explain)                           \
@@ -304,10 +307,14 @@ bool GMPChild::GetUTF8LibPath(nsACString& aOutLibPath) {
   }
 
   CopyUTF16toUTF8(path, aOutLibPath);
+#endif
   return true;
 }
 
 bool GMPChild::GetPluginName(nsACString& aPluginName) const {
+#ifdef MOZ_WIDGET_ANDROID
+  aPluginName = NS_ConvertUTF16toUTF8(mPluginPath);
+#else
   // Extract the plugin directory name if possible.
   nsCOMPtr<nsIFile> libFile;
   nsresult rv = NS_NewLocalFile(mPluginPath, getter_AddRefs(libFile));
@@ -322,6 +329,7 @@ bool GMPChild::GetPluginName(nsACString& aPluginName) const {
   NS_ENSURE_SUCCESS(rv, false);
 
   aPluginName.Assign(NS_ConvertUTF16toUTF8(parentLeafName));
+#endif
   return true;
 }
 
